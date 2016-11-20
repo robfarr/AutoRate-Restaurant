9f2e01cbccee808ebfb476c1087c337f109d54f8
Rob Farr <robfarr@users.noreply.github.com>
2016-11-20 01:24:07 +0100

    Setup local DB credentials for dev env.

--- src/environments/dev/common/config/main-local.php
+++ src/environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=selfierestaurantratings',
+            'username' => 'selfierestaurantratings',
+            'password' => 'selfierestaurantratings',
             'charset' => 'utf8',
         ],
         'mailer' => [
