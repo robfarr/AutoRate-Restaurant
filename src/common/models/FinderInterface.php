c0f45a97bae53e11700365e5ffcf5bbcd010f485
Brandon Hewer <brandonhewer@eduroam-2-217.epfl.ch>
2016-11-20 09:36:42 +0100

    i

--- src/common/models/FinderInterface.php
+++ src/common/models/FinderInterface.php
@@ -8,6 +8,8 @@ class FinderInterface {
 	private $entityID;
 	private $entityType;
 	private $client;
+	private $nearby_restaurants;
+	private $restaurants;
 	
 	public function __construct($latitude, $longitude) {
 		$this->latitude = $latitude;
@@ -17,7 +19,7 @@ class FinderInterface {
 	}
 	
 	public function establish_client() {
-		$url = 'https://api.projectoxford.ai/emotion/v1.0/recognize';
+		$url = 'https://www.zomato.com/api/v2.1/';
 		
 		$headers = array(
 			'Content-Type' => 'application/json',
@@ -27,8 +29,24 @@ class FinderInterface {
 		$this->client = new Client(array('headers' => $headers));
 	}
 	
-	public function find_location() {
+	public function get_restaurants() {
+		return $this->restaurants;
+	}
+	
+	private function find_location() {
 		
+	}
+	
+	private function parse_location($location_details) {
+		$this->restaurants=array();
+		$all_restaurants = $location_details->nearby_restaurants;
+		
+		foreach($all_restaurants as $value) {
+			$id = $all_restaurants->id;
+			$name = $all_restaurants->name;
+			$address = $all_restaurants->location->address;
+			$this->restaurants[] = array($id, $name, $address);
+		}
 	}
 }
 ?>
