96841d604d19c1291850644cea3cab44b781d296
Rob Farr <robfarr@users.noreply.github.com>
2016-11-20 01:44:02 +0100

    Display CTA & list of restaurants on index page.

--- src/frontend/views/site/index.php
+++ src/frontend/views/site/index.php
@@ -2,51 +2,52 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Rate a Restaurant &middot; Rate your restaurant meal by uploading a selfie.';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Rate a Restaurant</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Rate your restaurant meal by uploading a selfie.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="#">Rate Restaurant</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+            <table class="table table-hover">
+                <tbody>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+                    <tr>
+                        <th>Restaurant Name</th>
+                        <td>55% excited</td>
+                        <td><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></td>
+                    </tr>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                    <tr>
+                        <th>Restaurant Name</th>
+                        <td>55% excited</td>
+                        <td><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></td>
+                    </tr>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+                    <tr>
+                        <th>Restaurant Name</th>
+                        <td>55% excited</td>
+                        <td><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></td>
+                    </tr>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                    <tr>
+                        <th>Restaurant Name</th>
+                        <td>55% excited</td>
+                        <td><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></td>
+                    </tr>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+                </tbody>
+            </table>
+
         </div>
 
     </div>
