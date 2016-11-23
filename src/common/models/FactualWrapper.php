3c5762fc622f202ae4386d629fe172b79a0a126a
James Thompson <jamie.thompson@stellarise.com>
2016-11-23 20:48:02 +0000

    better code readablilty

--- src/common/models/FactualWrapper.php
+++ src/common/models/FactualWrapper.php
@@ -5,23 +5,17 @@ require ('factual-php-driver/Factual.php');
 
 class FactualWrapper {
 	
-	const RADIUS = 1000;
-	const RESTAURANT_ID = 347;
-	
-	private $client;
-	
-	public function __construct() {
-		$this->client = new \Factual(\Yii::$app->params['factualKey'], \Yii::$app->params['factualSecret']);
-	}
+	const SEARCH_RADIUS_METERS = 1000;
+	const RESTAURANT_CATEGORY_ID = 347;
 	
 	public function search($latitiude, $longitude) {
+		$factual = new \Factual(\Yii::$app->params['factualKey'], \Yii::$app->params['factualSecret']);
 		$query = new \FactualQuery();
-		$query->sortAsc("\$distance");
-		$query->select(["name", "address", "country", "factual_id", "locality", "post_town", "region", "postcode"]);
-		$query->field("category_ids")->includes(FactualWrapper::RESTAURANT_ID);
-		$query->within(new \FactualCircle($latitiude, $longitude, FactualWrapper::RADIUS));
-		$result = $this->client->fetch("places", $query);
-		return $this->combine($result->getData());
+		$query->sortAsc('$distance');
+		$query->select(["name", "address", "locality", "region", "postcode", "country", "factual_id"]);
+		$query->field("category_ids")->includes(FactualWrapper::RESTAURANT_CATEGORY_ID);
+		$query->within(new \FactualCircle($latitiude, $longitude, FactualWrapper::SEARCH_RADIUS_METERS));
+		return $this->combine($factual->fetch("places", $query)->getData());
 	}
 	
 	private function combine($response) {
@@ -30,7 +24,7 @@ class FactualWrapper {
 			$restaurant = new \stdClass();
 			$restaurant->name = $restaurantUnprocessed['name'];
 			$restaurant->address = 	(array_key_exists('address', $restaurantUnprocessed) ? $restaurantUnprocessed['address'] . ", " : "").
-									(array_key_exists('post_town', $restaurantUnprocessed) ? $restaurantUnprocessed['post_town'] . ", " : "").
+									(array_key_exists('locality', $restaurantUnprocessed) ? $restaurantUnprocessed['locality'] . ", " : "").
 									(array_key_exists('region', $restaurantUnprocessed) ? $restaurantUnprocessed['region'] . ", " : "") .
 									(array_key_exists('postcode', $restaurantUnprocessed) ? ($restaurantUnprocessed['postcode'] . ", ") : "").
 									(array_key_exists('country', $restaurantUnprocessed) ? Countries::getCountry($restaurantUnprocessed['country']) : "");
